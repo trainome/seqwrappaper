5507e2de0b68a0fbf74fa52cb725d527e41b805d
Daniel Hammarström <daniel.hammarstrom@inn.no>
2026-05-19 14:52:28 +0200

    fix in figure-opts.R

--- analysis/figures/figure-opts.R
+++ analysis/figures/figure-opts.R
@@ -4,6 +4,3 @@
 colors <- c("#1b9e77", "#d95f02", "#7570b3", "#e7298a", "#66a61e")
 
 # colors <- c("#CA0020", "#F4A582", "#F7F7F7", "#92C5DE", "#0571B0")
-
-# Check if data-out exists - This is needed to store temp data for figures
-if (!dir.exists("data-out")) dir.create("data-out")
