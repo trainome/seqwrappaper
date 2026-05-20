bb3aab6c7dcb8f9f3e2f41312805f905cdd4bb4c
Daniel Hammarström <daniel.hammarstrom@inn.no>
2026-05-20 14:58:48 +0200

    upd pkg

--- data-raw/pillon_counts.R
+++ data-raw/pillon_counts.R
@@ -5,26 +5,26 @@
 # muscle of people with type 2 diabetes. Sci Adv 8(36):eabo3192.
 #
 
-brary(GEOquery)
+library(GEOquery)
 
 # Get the gene counts
 counts <- getRNASeqData("GSE202295")
 
 # Sort row annotations and convert to symbols
 annotation <- SummarizedExperiment::rowData(counts) |>
-  darame() |>
-  dplyr:ect(GeneID, Symbol) |>
-  dplyr::mutGeneID = as.character(GeneID)) |>
-  data.frame(rowes = NULL)
+  data.frame() |>
+  dplyr::select(GeneID, Symbol) |>
+  dplyr::mutate(GeneID = as.character(GeneID)) |>
+  data.frame(row.names = NULL)
 
 # Get the count matrix and add symbols
 counts_mat <- counts@assays@data@listData$counts
 
 countdata <- counts_mat |>
   data.frame() |>
-  le::rownames_to_co("GeneID") |>
-  dplyr::inner_join(annotati|>
-  dplyr::select(geneid = Symbol,yselect::starts_with("GSM"))
+  tibble::rownames_to_column("GeneID") |>
+  dplyr::inner_join(annotation) |>
+  dplyr::select(geneid = Symbol, tidyselect::starts_with("GSM"))
 
 # Remove all zero row counts
 countdata <- countdata[rowSums(countdata[, -1]) != 0, ]
@@ -33,13 +33,13 @@ countdata <- countdata[rowSums(countdata[, -1]) != 0, ]
 # Sort meta data
 metadata <- counts@colData |>
   data.frame() |>
-  dplyr::select(semple_id = geo_accen, group_id_time = title) |>
-  tidyr::separate(group_id_time, into = c("g", "id", "time")) |>
-  dplyr::mutate(time = factor(time, levels = c("l", "post", "rec"))) |>
+  dplyr::select(seq_sample_id = geo_accession, group_id_time = title) |>
+  tidyr::separate(group_id_time, into = c("group", "id", "time")) |>
+  dplyr::mutate(time = factor(time, levels = c("basal", "post", "rec"))) |>
   data.frame(row.names = NULL)
 
 
-# Save the data as file
+# Save the data as one file
 pillon_counts <- list(metadata = metadata, countdata = countdata)
 
 usethis::use_data(pillon_counts, compress = "xz", overwrite = TRUE)
